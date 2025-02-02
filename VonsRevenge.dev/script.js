57a85c3daea4dbf7ea2628ab899370bb3230526d
Von <164780172+vonsrevenge@users.noreply.github.com>
2025-02-02 21:32:21 +0200

    Update script.js

--- VonsRevenge.dev/script.js
+++ VonsRevenge.dev/script.js
@@ -14,3 +14,23 @@ document.addEventListener("DOMContentLoaded", () => {
 
     sections.forEach((section) => observer.observe(section));
 });
+
+// Disable right-click
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+
+function ctrlShiftKey(e, keyCode) {
+return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+}
+
+document.onkeydown = (e) => {
+// Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+if (
+e.keyCode === 123 || // F12
+ctrlShiftKey(e, 'I') || // Ctrl + Shift + I
+ctrlShiftKey(e, 'J') || // Ctrl + Shift + J
+ctrlShiftKey(e, 'C') || // Ctrl + Shift + C
+(e.ctrlKey && e.keyCode === 'U'.charCodeAt(0)) // Ctrl + U
+) {
+return false;
+}
+};
